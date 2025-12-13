8ca81cf325bd622476c6805654ae7536be7e7c35
ANUJ SINGH <singhanuj53883@gmail.com>
2025-12-13 18:47:33 +0530

    map location changed

--- ChatApp/latest/src/dashboard.jsx
+++ ChatApp/latest/src/dashboard.jsx
@@ -165,18 +165,7 @@ width:'80%'
 }
 
 
-let iterate= getmessage.map((v,i)=>{
-  return(
-    <div key={i} style={(getid==v.RecieverId)?color:nocolor}>
- <p style={{margin:'0px'}}>{v.message}</p>
- <div className="messagetime">
- <p style={{margin:'0px'}}>{new Date(v.createdAt).toLocaleDateString('en-IN')}</p>
- <p style={{margin:'0px'}}>{new Date(v.createdAt).toLocaleTimeString('en-IN')}</p>
- </div>
 
-    </div> 
-  )
-})
 
 
 
@@ -223,8 +212,18 @@ let iterate= getmessage.map((v,i)=>{
       </div>
       <div className='chatarea'>   
         {       
-(getmessage.length!=0&&active)?iterate:
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><radialGradient id="a12" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)"><stop offset="0" stop-color="#44061D"></stop><stop offset=".3" stop-color="#44061D" stop-opacity=".9"></stop><stop offset=".6" stop-color="#44061D" stop-opacity=".6"></stop><stop offset=".8" stop-color="#44061D" stop-opacity=".3"></stop><stop offset="1" stop-color="#44061D" stop-opacity="0"></stop></radialGradient><circle transform-origin="center" fill="none" stroke="url(#a12)" stroke-width="13" stroke-linecap="round" stroke-dasharray="200 1000" stroke-dashoffset="0" cx="100" cy="100" r="70"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></circle><circle transform-origin="center" fill="none" opacity=".2" stroke="#44061D" stroke-width="13" stroke-linecap="round" cx="100" cy="100" r="70"></circle></svg>
+(getmessage.length!=0&&active)? getmessage.map((v,i)=>{
+  return(
+    <div key={i} style={(getid==v.RecieverId)?color:nocolor}>
+ <p style={{margin:'0px'}}>{v.message}</p>
+ <div className="messagetime">
+ <p style={{margin:'0px'}}>{new Date(v.createdAt).toLocaleDateString('en-IN')}</p>
+ <p style={{margin:'0px'}}>{new Date(v.createdAt).toLocaleTimeString('en-IN')}</p>
+ </div>
+
+    </div> 
+  )
+}):<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 200"><radialGradient id="a12" cx=".66" fx=".66" cy=".3125" fy=".3125" gradientTransform="scale(1.5)"><stop offset="0" stop-color="#44061D"></stop><stop offset=".3" stop-color="#44061D" stop-opacity=".9"></stop><stop offset=".6" stop-color="#44061D" stop-opacity=".6"></stop><stop offset=".8" stop-color="#44061D" stop-opacity=".3"></stop><stop offset="1" stop-color="#44061D" stop-opacity="0"></stop></radialGradient><circle transform-origin="center" fill="none" stroke="url(#a12)" stroke-width="13" stroke-linecap="round" stroke-dasharray="200 1000" stroke-dashoffset="0" cx="100" cy="100" r="70"><animateTransform type="rotate" attributeName="transform" calcMode="spline" dur="2" values="360;0" keyTimes="0;1" keySplines="0 0 1 1" repeatCount="indefinite"></animateTransform></circle><circle transform-origin="center" fill="none" opacity=".2" stroke="#44061D" stroke-width="13" stroke-linecap="round" cx="100" cy="100" r="70"></circle></svg>
         }   
 
       </div>
